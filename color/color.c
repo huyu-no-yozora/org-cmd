0d195d30cad73b8e943c274b2a6cddbb4eaff83a
huyu-no-yozora <60916159+huyu-no-yozora@users.noreply.github.com>
2020-08-09 02:47:21 +0900

    [put] initial code was putted

---
+++ color/color.c
@@ -0,0 +1,17 @@
+// GNU General Public License
+/*
+ *  color/color.c
+ *
+ *  Copyright (c) 2020 冬ノ夜空
+ */
+
+#include <stdio.h>
+#include "include/color.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
+
---
+++ color/include/color.h
@@ -0,0 +1,14 @@
+// GNU General Public License
+/*
+ *  color/include/color.h
+ *
+ *  Copyright (c) 2020 冬ノ夜空
+ */
+
+#ifndef color_h
+#define color_h
+
+//#include <stdio.h>
+
+#endif /* color_h */
+
